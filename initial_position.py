1f42765fd0c49291de3d31bfcc1bf3a11b671155
irunegoizueta <36876045+irunegoizueta@users.noreply.github.com>
2018-03-11 12:47:18 +0100

    Add files via upload

---
+++ initial_position.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2013, SRI International
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of SRI International nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Acorn Pooley
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import time
+import roslib; roslib.load_manifest('ur_driver')
+import rospy
+import actionlib
+import math
+from control_msgs.msg import *
+from trajectory_msgs.msg import *
+from sensor_msgs.msg import JointState
+from math import pi
+from moveit_msgs.msg import RobotTrajectory
+from moveit_msgs.msg import *
+from trajectory_msgs.msg import JointTrajectoryPoint
+from geometry_msgs.msg import PoseStamped, Pose
+
+from std_msgs.msg import String
+
+def scale_trajectory_speed(traj, scale):
+       # Create a new trajectory object
+       new_traj = RobotTrajectory()
+      
+       # Initialize the new trajectory to be the same as the planned trajectory
+       new_traj.joint_trajectory = traj.joint_trajectory
+      
+       # Get the number of joints involved
+       n_joints = len(traj.joint_trajectory.joint_names)
+      
+       # Get the number of points on the trajectory
+       n_points = len(traj.joint_trajectory.points)
+       
+       # Store the trajectory points
+       points = list(traj.joint_trajectory.points)
+
+       # Cycle through all points and scale the time from start, speed and acceleration
+       for i in range(n_points):
+           point = JointTrajectoryPoint()
+           point.time_from_start = traj.joint_trajectory.points[i].time_from_start / scale
+           point.velocities = list(traj.joint_trajectory.points[i].velocities)
+           point.accelerations = list(traj.joint_trajectory.points[i].accelerations)
+           point.positions = traj.joint_trajectory.points[i].positions
+                         
+           for j in range(n_joints):
+               point.velocities[j] = point.velocities[j] * scale
+               point.accelerations[j] = point.accelerations[j] * scale * scale
+            
+           points[i] = point
+
+       # Assign the modified points to the new trajectory
+       new_traj.joint_trajectory.points = points
+
+       # Return the new trajecotry
+       return new_traj
+
+def move_robot():
+
+  ## First initialize moveit_commander and rospy.
+  print "============ Starting tutorial setup"
+  moveit_commander.roscpp_initialize(sys.argv)
+  rospy.init_node('move_ur10_robot',
+                  anonymous=True)
+
+  ## Instantiate a RobotCommander object.  This object is an interface to
+  ## the robot as a whole.
+  robot = moveit_commander.RobotCommander()
+
+  ## Instantiate a PlanningSceneInterface object.  This object is an interface
+  ## to the world surrounding the robot.
+  scene = moveit_commander.PlanningSceneInterface()
+
+  ## Instantiate a MoveGroupCommander object.  This object is an interface
+  ## to one group of joints. 
+  group = moveit_commander.MoveGroupCommander("manipulator")
+  
+  display_trajectory_publisher = rospy.Publisher(
+                                      '/move_group/display_planned_path',
+                                      moveit_msgs.msg.DisplayTrajectory,
+                                      queue_size=20)
+  print "============ Waiting for RVIZ..."
+  rospy.sleep(5)
+  print "============ Starting tutorial "
+
+  
+
+  ## Add collision object
+  obj_pose = geometry_msgs.msg.PoseStamped()
+  obj_pose.header.frame_id = robot.get_planning_frame()
+  obj_pose.pose.position.x = 0.
+  obj_pose.pose.position.y = 0.
+  obj_pose.pose.position.z = -0.2
+  scene.add_box("table", obj_pose, (2.0, 1.5, 0.25)) # x_axis, y_axis, z_axis
+
+  ## Getting Basic Information
+  ## ^^^^^^^^^^^^^^^^^^^^^^^^^
+  ##
+  ## We can get the name of the reference frame for this robot
+  print "============ Reference frame: %s" % group.get_planning_frame()
+
+  ## We can also print the name of the end-effector link for this group
+  print "============ End effector: %s" % group.get_end_effector_link()
+
+  ## We can get a list of all the groups in the robot
+  print "============ Robot Groups:"
+  print robot.get_group_names()
+
+  ## Sometimes for debugging it is useful to print the entire state of the
+  ## robot.
+  print "============ Printing robot state"
+  print robot.get_current_state()
+  print "============"
+
+  ##codigo para poner el brazo con el end effector vertical
+  group_variable_values = group.get_current_joint_values()
+  group_variable_values[0] = pi*25/180#pi*3.46/180 #0
+  group_variable_values[1] = -pi*108.28/180 #-pi*3/9
+  group_variable_values[2] = -pi*107.5/180 #-pi*6/9
+  group_variable_values[3] = -pi*53.72/180 #-pi*3/2-(group_variable_values[1]+group_variable_values[2])
+  group_variable_values[4] = pi*1/2 #igual
+  group_variable_values[5] = -pi*176.47/180#-pi/2 #igual
+  group.set_joint_value_target(group_variable_values)
+  plan = group.plan()
+  rospy.sleep(2)
+  scaled_traj2 = scale_trajectory_speed(plan, 0.15)
+  group.execute(scaled_traj2)
+  ##hasta aqui ende effector vertical
+
+  rospy.sleep(5)
+
+  ## When finished shut down moveit_commander.
+  moveit_commander.roscpp_shutdown()
+
+  ## END_TUTORIAL
+
+  print "============ STOPPING"
+
+
+if __name__=='__main__':
+  try:
+    move_robot()
+  except rospy.ROSInterruptException:
+    pass
+moveit_commander.os._exit(0)
